e3e19fff60f0b9e15908dc43a5e6fa531d430ca0
Muhammad Firdaus <mbex71@gmail.com>
2024-11-05 07:59:39 +0700

    feat - add filament role permission plugin

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use SolutionForest\FilamentAccessManagement\Concerns\FilamentUserHelpers;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,FilamentUserHelpers;
+    
 
     /**
      * The attributes that are mass assignable.
