a3b00c13f712269334fab9265e0bf25eda4591b7
Muhammad Firdaus <mbex71@gmail.com>
2024-11-06 09:45:58 +0700

    feat - warehouse  seeder

---
+++ database/seeders/WarehouseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Warehouse;
+
+class WarehouseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+        Warehouse::upsert(
+            [
+                [
+                    'name' => 'Polres Gresik',
+                    'location' => 'Gresik',
+                ],
+                [
+                    'name' => 'Polres Sidoarjo',
+                    'location' => 'Sidoarjo',
+                ],
+            ],
+            ['id']
+        );
+    }
+}
