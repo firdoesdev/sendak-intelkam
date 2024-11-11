ead59c4ab535c6ff16ea731cac6a8d08dc109466
Muhammad Firdaus <mbex71@gmail.com>
2024-11-11 21:22:02 +0700

    feat - rekom type master data

---
+++ app/Filament/Resources/RekomTypeResource/Pages/CreateRekomType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\RekomTypeResource\Pages;
+
+use App\Filament\Resources\RekomTypeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRekomType extends CreateRecord
+{
+    protected static string $resource = RekomTypeResource::class;
+}
---
+++ app/Models/RekomType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RekomType extends Model
+{
+    /** @use HasFactory<\Database\Factories\RekomTypeFactory> */
+    use HasFactory;
+}
