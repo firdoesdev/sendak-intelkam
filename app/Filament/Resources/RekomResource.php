6b8b5c4bb0b553be7347605e678a80cc85f4157f
Muhammad Firdaus <firdoes.dev@gmail.com>
2024-11-13 17:00:59 +0700

    wip - form create rekom

--- app/Filament/Resources/RekomResource.php
+++ app/Filament/Resources/RekomResource.php
@@ -5,14 +5,24 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\RekomResource\Pages;
 use App\Filament\Resources\RekomResource\RelationManagers;
 use App\Models\Rekom;
-
+use Filament\Actions\EditAction;
 use Filament\Forms;
+use Filament\Forms\Components\Actions;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Split;
 use Filament\Forms\Form;
+use Filament\Infolists;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Infolists\Infolist;
+use Filament\Infolists\Components\TextEntry;
+
+use Filament\Forms\Components\Placeholder;
+use Illuminate\Support\HtmlString;
+
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -25,35 +35,76 @@ class RekomResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+
     public static function form(Form $form): Form
     {
 
-        dump(auth()->user()->hasRole('super-admin'));
+        // dump(auth()->user()->hasRole('super-admin'));
         return $form
             ->schema([
-                //
-                // Forms\Components\Select::make('role.name')
-                // ->relationship('role', 'name')
-                // ->options(Role::all()->pluck('name', 'id')->toArray()),
+                Grid::make(12)->schema([
 
-                // Forms\Components\TextInput::make('no_rekom'),
-                Section::make([
-                    Forms\Components\TextInput::make('title'),
-                    Forms\Components\Textarea::make('content'),
+                    Fieldset::make('Data Pemilik')->schema([
+                        Forms\Components\Select::make('no_rekom')
+                            ->label('Jenis Kepemilikan')
+                            ->options([
+                                'Perorangan' => 'Perorangan',
+                                'Perusahaan' => 'Perusahaan'
+                            ])
+                            ->required(),
+                        Forms\Components\TextInput::make('no_rekom')
+                            ->label('NIK')
+                            ->required(),
+                        Forms\Components\TextInput::make('no_rekom')
+                            ->label('Nama')
+                            ->required(),
+                        Forms\Components\TextInput::make('no_rekom')
+                            ->label('Alamat')
+                            ->required(),
+                        Forms\Components\TextInput::make('no_rekom')
+                            ->label('Nomor Telepon')
+                            ->required(),
+                    ])->columnSpan(8),
+
+                    Fieldset::make('Masa Berlaku')->schema([
+
+                        Placeholder::make('Masa Berlaku')
+                        ->content(
+                            now()),
+                       
+                        Forms\Components\DatePicker::make('no_rekom')
+                        ->label('Masa Berlaku')
+                        ->default(now())
+                        ->columnSpanFull(),
+                        
+                        Forms\Components\Actions::make([
+                            Forms\Components\Actions\Action::make('Update Masa Berlaku')
+                                
+                                ->action(function (Forms\Get $get, Forms\Set $set) {
+                                    $set('excerpt', str($get('content'))->words(45, end: ''));
+                                })->requiresConfirmation()
+                        ])
+                        ->columnSpanFull(),
+                        
+                        
+                    ])
+                    ->columnSpan(4),
+
+                    Fieldset::make('Data Senjata')->schema([
+                        Forms\Components\TextInput::make('no_rekom')
+                            ->label('Nomor Seri Senjata'),
+                        Forms\Components\Select::make('no_rekom')
+                            ->label('Jenis Senjata')
+                            ->options([
+                                'Perorangan' => 'Perorangan',
+                                'Perusahaan' => 'Perusahaan'
+                            ]),
+                        Forms\Components\TextInput::make('no_rekom')
+                            ->label('Kaliber'),
+                    ])->columnSpan(8),
+
+
                 ]),
-
-                Split::make([
-                    Section::make([
-                        Forms\Components\TextInput::make('title'),
-                        Forms\Components\Textarea::make('content'),
-                    ])->maxWidth('md'),
-                    Section::make([
-                        Forms\Components\Toggle::make('is_published'),
-                        Forms\Components\Toggle::make('is_featured'),
-                    ])->grow(false),
-                ])->from('md')
-
-                
             ]);
     }
 
