215cccafd9ee9cc6cec13738ec256bd81e85cf75
Muhammad Firdaus <firdoes.dev@gmail.com>
2024-11-20 17:27:13 +0700

    feat - add relation rekom & role

--- app/Filament/Resources/OwnerResource/RelationManagers/RekomsRelationManager.php
+++ app/Filament/Resources/OwnerResource/RelationManagers/RekomsRelationManager.php
@@ -3,12 +3,15 @@
 namespace App\Filament\Resources\OwnerResource\RelationManagers;
 
 use Filament\Forms;
+use Filament\Forms\Components\BelongsToSelect;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Spatie\Permission\Models\Role;
 
 class RekomsRelationManager extends RelationManager
 {
@@ -21,6 +24,12 @@ class RekomsRelationManager extends RelationManager
                 Forms\Components\TextInput::make('no_rekom')
                     ->required()
                     ->maxLength(255),
+                BelongsToSelect::make('rekom_type_id')
+                ->relationship('rekomType', 'name'),
+                BelongsToSelect::make('role_id')
+                ->relationship('role', 'name')
+                // ->options(Role::all()->pluck('name', 'id'))
+                // ->default(R),
             ]);
     }
 
@@ -30,6 +39,8 @@ class RekomsRelationManager extends RelationManager
             ->recordTitleAttribute('no_rekom')
             ->columns([
                 Tables\Columns\TextColumn::make('no_rekom'),
+                Tables\Columns\TextColumn::make('role.name')
+                ->label('Divisi'),
             ])
             ->filters([
                 //
