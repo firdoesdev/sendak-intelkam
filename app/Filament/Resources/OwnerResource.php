5ba8a43bfc855382c72505f272d2cb7c512dc0da
Muhammad Firdaus <firdoes.dev@gmail.com>
2024-11-27 10:27:37 +0700

    feat - field document

--- app/Filament/Resources/OwnerResource.php
+++ app/Filament/Resources/OwnerResource.php
@@ -9,7 +9,13 @@ use App\Models\Owner;
 use App\Models\OwnerType;
 use Filament\Forms\Components\BelongsToSelect;
 use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Tabs;
+
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -25,15 +31,15 @@ class OwnerResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function getEloquentQuery(): Builder
-    {   
+    {
         return parent::getEloquentQuery()
-        ->with(['rekoms'])
-        ->whereHas('rekoms', function ($query) {    
-            $rekoms = new CommonRekomService();
-            return$query->where('role_id', $rekoms->getRekomRoleId());
-        });
+            ->with(['rekoms'])
+            ->whereHas('rekoms', function ($query) {
+                $rekoms = new CommonRekomService();
+                return $query->where('role_id', $rekoms->getRekomRoleId());
+            });
     }
-    
+
 
     public static function form(Form $form): Form
     {
@@ -42,40 +48,79 @@ class OwnerResource extends Resource
 
         return $form
             ->schema([
-                Fieldset::make('Data Kepemilikan')
-                        ->columnSpan(8)
-                        ->schema([
-                            BelongsToSelect::make('ownerType')
-                            ->label('Jenis Kepemilikan')
-                            ->relationship('ownerType', 'name')
-                            ->default($defaultOwnerTypeId)
-                            ->disabled()
-                            ->required()
-                            ->hidden(),
-                            
-                            TextInput::make('name')
-                            ->placeholder('ex: John Doe')
-                            ->required(),
-                            
-                            TextInput::make('no_ktp')
-                            ->label('Nomor KTP')
-                            ->numeric()
-                            ->placeholder('ex: 9999999999999999')
-                            ->required(),
+                Tabs::make('Tabs')
+                    ->columnSpanFull()
+                    ->tabs([
+                        Tab::make('Data Kepemilikan')->schema([
+                            Grid::make()->schema([
+                                BelongsToSelect::make('ownerType')
+                                    ->label('Jenis Kepemilikan')
+                                    ->relationship('ownerType', 'name')
+                                    ->default($defaultOwnerTypeId)
+                                    ->disabled()
+                                    ->required()
+                                    ->hidden(),
 
-                            TextInput::make('address')
-                            ->label('Alamat')
-                            ->placeholder('ex: Jalan Raya No. 1')
-                            ->required(),
+                                TextInput::make('name')
+                                    ->placeholder('ex: John Doe')
+                                    ->required(),
 
-                            TextInput::make('phone')
-                            ->label('Nomor Telepon')
-                            ->placeholder('ex: 08123456789')
-                            ->numeric()
-                            ->required(),
-                            
+                                TextInput::make('no_ktp')
+                                    ->label('Nomor KTP')
+                                    ->numeric()
+                                    ->placeholder('ex: 9999999999999999')
+                                    ->required(),
+
+                                TextInput::make('address')
+                                    ->label('Alamat')
+                                    ->placeholder('ex: Jalan Raya No. 1')
+                                    ->required(),
+
+                                TextInput::make('phone')
+                                    ->label('Nomor Telepon')
+                                    ->placeholder('ex: 08123456789')
+                                    ->numeric()
+                                    ->required(),
+
+                            ]),
+
+
                         ]),
-                    ]);
+                        Tab::make('Dokumen Kepemilikan')->schema([
+                            Grid::make(3)->schema([
+                                FileUpload::make('ktp')
+                                    ->label('KTP'),
+                                FileUpload::make('npwp')
+                                    ->label('NPWP'),
+                                FileUpload::make('ksk')
+                                    ->label('KSK'),
+                                FileUpload::make('skep_jabatan')
+                                    ->label('Skep Jabatan'),
+                                FileUpload::make('kta')
+                                    ->label('KTA'),
+                                FileUpload::make('si_impor')
+                                    ->label('Surat Ijin Impor')
+
+                            ]),
+
+                        ]),
+                        Tab::make('Dokumen Hasil Tes')
+                            ->schema([
+                                Grid::make(3)->schema([
+                                    FileUpload::make('health_certificate')
+                                        ->label('Hasil Tes Kesehatan'),
+                                    FileUpload::make('psychological_certificate')
+                                        ->label('Hasil Tes Psikologi'),
+                                    FileUpload::make('shooting_certificate')
+                                        ->label('Hasil Tes Menembak'),
+
+
+
+                                ])
+                            ])
+                    ])
+
+            ]);
     }
 
     public static function table(Table $table): Table
@@ -87,15 +132,15 @@ class OwnerResource extends Resource
                 Tables\Columns\TextColumn::make('phone')->searchable()->sortable(),
                 Tables\Columns\TextColumn::make('address')->searchable()->sortable(),
                 Tables\Columns\TextColumn::make('rekoms.no_rekom')
-                ->label('No Rekom'),
+                    ->label('No Rekom'),
                 Tables\Columns\TextColumn::make('rekoms.activated_at')
-                ->label('Tanggal Rekom Terbit')
-                ->date(),               
+                    ->label('Tanggal Rekom Terbit')
+                    ->date(),
                 Tables\Columns\TextColumn::make('rekoms.expired_at')
-                ->label('Tanggal Rekom Terbit')
-                ->date(),               
+                    ->label('Tanggal Rekom Terbit')
+                    ->date(),
                 Tables\Columns\TextColumn::make('rekoms.role.name')
-                ->label('Divisi')
+                    ->label('Divisi')
 
             ])
             ->filters([
@@ -103,7 +148,7 @@ class OwnerResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make()
-                ->label('Ubah'),
+                    ->label('Ubah'),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
